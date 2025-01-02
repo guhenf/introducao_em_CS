1caa6e492b2fc686ea4aa95a5a9522ce6e4924d8
Guhenf <guhenf@gmail.com>
2025-01-02 18:46:17 -0300

    Teste HelloWord  com MSTest

---
+++ Exercicios.Tests/Test1.cs
@@ -0,0 +1,15 @@
+﻿using Exercicios.Domain;
+
+namespace Exercicios.Tests
+{
+[TestClass]
+public class HelloWorldTest
+    {
+        [TestMethod]
+        public void SayHelloTest()
+        {
+           string msg = HelloWorld.SayHello();
+           Assert.AreEqual("Hello World!", msg);
+        }
+    }
+}
